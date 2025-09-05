d01d0914d4300e1864c3bf386f153bd3d6a698fa
simran-123-dev <kaursimran192007@gmail.com>
2025-09-05 21:15:56 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,10 +1,24 @@
-function getWeather() {
+async function getWeather() {
   const city = document.getElementById("cityInput").value;
   if (city === "") {
     document.getElementById("result").textContent = "Please enter a city!";
   } else {
-    // Demo ke liye fake weather
-    document.getElementById("result").textContent = 
-      `Weather in ${city}: Sunny, 30°C ☀️`;
+    const apiKey = "6181a134f6a89be4b452906e3540c91b"; // tumhari API key
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error("City not found");
+
+      const data = await response.json();
+      const temp = data.main.temp;
+      const condition = data.weather[0].description;
+
+      document.getElementById("result").textContent =
+        `Weather in ${city}: ${condition}, ${temp}°C 🌤️`;
+    } catch (error) {
+      document.getElementById("result").textContent =
+        "❌ Invalid city name or API issue";
+    }
   }
 }
